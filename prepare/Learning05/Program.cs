48c679f0a6a814c9a0d9d1ba6c409e4ae17f55c0
jaschonb <jaschonbaumann@gmail.com>
2023-07-03 12:56:12 -0600

    Completed learning05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square squ1 = new Square("Blue", 2.3);
+        Rectangle rec1 = new Rectangle("Red", 1.3, 2.7);
+        Circle cir1 = new Circle("Green", 2.4);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(squ1);
+        shapes.Add(rec1);
+        shapes.Add(cir1);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }        
     }
 }
